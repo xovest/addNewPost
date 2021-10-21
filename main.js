7f9f0f5f714f59ead15af447d6995532fcae0497
xovest <79008917+xovest@users.noreply.github.com>
2021-10-21 16:30:21 +0300

    just put some comments cuz im new and noob and everything and cant figure out

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-//make require work
+const postsList = document.querySelector('.posts-list');
+
+/*
+const url = 'http://localhost:5500/api/posts';
+ That url doesn't work, need to see what's up with that cors error and how I can configure that package already so that it works, but dunno about the mongo still, don't wanna bother now since i'm noob
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+*/
